14976197cfdc83c107784cc4270e253de495a95d
Willem Essenstam <31310808+wessenstam@users.noreply.github.com>
2021-02-25 20:21:45 +0100

    Change AWS name to the correct naming

--- scripts/lib.pe.api.sh
+++ scripts/lib.pe.api.sh
@@ -403,9 +403,6 @@ function pe_init_aws_api() {
   #log "Prism Central: |${PC_HOST}|"
   #log "Prism Element: |${PE_HOST}|"
 
-  # Set the AWS IP address to PE_HOST
-  AWScluster=$PE_HOST
-
   #############################################################
   # Set the Cluster Name
   #############################################################
@@ -423,8 +420,10 @@ function pe_init_aws_api() {
   cluster_uuid=$(curl ${CURL_HTTP_OPTS} --user ${PRISM_ADMIN}:${PE_PASSWORD} -X GET -d '{}' "https://$PE_HOST:9440/PrismGateway/services/rest/v1/cluster" | jq '.uuid' | tr -d \")
   cluster_ip=$(curl ${CURL_HTTP_OPTS} --user ${PRISM_ADMIN}:${PE_PASSWORD} -X GET -d '{}' "https://$PE_HOST:9440/PrismGateway/services/rest/v1/cluster" | jq '.clusterExternalIPAddress' | tr -d \")
   cluster_dns=$(curl ${CURL_HTTP_OPTS} --user ${PRISM_ADMIN}:${PE_PASSWORD} -X GET -d '{}' "https://$PE_HOST:9440/PrismGateway/services/rest/v1/cluster" | jq '.nameServers[]' | tr -d \")
-  cluster_name="AWS-Cluster"
-
+  cluster_name_tmp=$(curl ${CURL_HTTP_OPTS} --user ${PRISM_ADMIN}:${PE_PASSWORD} -X GET -d '{}' "https://$PE_HOST:9440/PrismGateway/services/rest/v1/cluster" | jq '.name' | tr -d \")
+  
+  # Set the name of the Aws Cluster to AWS-Cluster-xxxx by stripping the Cluster-HPOC from its original name
+  cluster_name="${cluster_name_tmp:0:4}Cluster${cluster_name_tmp:16}"
 
   log "Cluster ID: |${cluster_id}|"
   log "Cluster UUID: |${cluster_uuid}|"
